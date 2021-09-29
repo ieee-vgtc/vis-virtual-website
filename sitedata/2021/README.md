ae26cebe88adb29aa080d4425d2e8a6b8d6172fe
Alper Sarikaya <yelper@gmail.com>
2021-09-28 20:59:18 -0700

    dynamic calendar

--- sitedata/2021/README.md
+++ sitedata/2021/README.md
@@ -16,6 +16,6 @@ Feel free to visit [our Help page above](/help.html) for answers to common quest
 
 The IEEE VIS 2021 Virtual site was adapted from the MiniConf software by the [IEEE VIS 2021 website, technology, and archive commmittees](http://ieeevis.org/year/2021/info/committees/conference-committee) with content input from [all conference committees](http://ieeevis.org/year/2021/info/committees/conference-committee).
 
-MiniConf was originally built by [Hendrik Strobelt](http://twitter.com/hen_str) and [Sasha Rush](http://twitter.com/srush_nlp); you can find [the original open-source implementation on GitHub](https://github.com/Mini-Conf/Mini-Conf), and [our fork as well](https://github.com/ieee-vgtc/vis-virtual-website) (which will become public after the conclusion of the conference).
+MiniConf was originally built by [Hendrik Strobelt](http://twitter.com/hen_str) and [Sasha Rush](http://twitter.com/srush_nlp); you can find [the original open-source implementation on GitHub](https://github.com/Mini-Conf/Mini-Conf), and [our fork as well](https://github.com/ieee-vgtc/vis-virtual-website).
 
-We welcome your comments about your experience and anything we can do to make your virtual experience better.  Please join us in #support or #suggestions on Discord, or feel free to e-mail the committees directly.
+We welcome your comments about your experience and anything we can do to make your virtual experience better.  Please join us in #support or #suggestions on Discord, or feel free to e-mail the committees directly (e.g., "web" at the conference domain).
--- static/2021/js/views/calendar.js
+++ static/2021/js/views/calendar.js
@@ -1,7 +1,13 @@
-function updateCalendar() {
-  updateTzDropdown();
-  updateTimezone();
+function finishCalendar(renderPromises) {
   updateKey();
+  updateTzDropdown();
+
+  renderPromises.push(updateFullCalendar());
+
+  // only when everything has rendered do we update times in the calendar
+  Promise.all(renderPromises).then(() => {
+    updateTimezone();
+  });
 }
 
 function getTimezone() {
@@ -14,20 +20,20 @@ function getTimezone() {
   return moment.tz.guess();
 }
 
-// ...promise fulfilled after cal is rendered.
 function updateTzDropdown() {
-  const current_tz = getUrlParameter('tz') || window.localStorage.getItem(
-    "tz") || moment.tz.guess();
+  const current_tz = getUrlParameter('tz') || window.localStorage.getItem("tz") || moment.tz.guess();
   const tzNames = [...moment.tz.names()];
 
   const setupTZSelector = () => {
-    const tzOptons = d3.select('#tzOptions')
+    const tzOptons = d3.selectAll('select.tzOptions')
     tzOptons.selectAll('option').data(tzNames)
       .join('option')
       .attr('data-tokens', d => d.split("/").join(" "))
-      .text(d => d)
+      .attr('selected', d => d === current_tz ? true : null)
+      .text(d => d);
+
     $('.selectpicker')
-      .selectpicker('val', current_tz)
+      .selectpicker({ liveSearch: true })
       .on('changed.bs.select',
         function (e, clickedIndex, isSelected, previousValue) {
           new_tz = tzNames[clickedIndex];
@@ -36,9 +42,9 @@ function updateTzDropdown() {
           localStorage.setItem("tz", new_tz);
 
           window.open(window.location.pathname + '?tz=' + new_tz, '_self');
-        })
+        });
 
-    $("#saveTz").on('click', function (_) {
+    $(".saveTz").on('click', function (_) {
       const new_tz = $(".selectpicker").val();
 
       const localStorage = window.localStorage;
@@ -47,7 +53,7 @@ function updateTzDropdown() {
       window.open(window.location.pathname + '?tz=' + new_tz, '_self');
     });
 
-    $("#resetTz").on('click', function (_) {
+    $(".resetTz").on('click', function (_) {
       const localStorage = window.localStorage;
       localStorage.removeItem("tz");
       window.open(window.location.pathname, '_self');
@@ -96,6 +102,178 @@ function updateKey() {
   });
 }
 
+function updateFullCalendar(day) {
+  let calendar = d3.select(`#calendar${day != null ? "-" + day : ""}`);
+  let allSessions = calendar.selectAll('g.all-sessions')
+    .data([1])
+    .join('div')
+      .attr('class', 'all-sessions');
+  allSessions.selectAll('.day').remove();
+
+  let calendar_json = "serve_main_calendar.json";
+
+  // are we making the full calendar or an individual day's calendar?
+  if (day != null) {
+    calendar_json = `serve_calendar_${day}.json`;
+    populateRooms(calendar);
+    populateTimes(calendar);
+  }
+  else {
+    populateDays(calendar);
+    populateTimes(calendar);
+  }
+
+  // return deferred promise
+  return $.when($.get('serve_config.json'), $.get(calendar_json))
+    .done((config, events) => {
+      if (day != null)
+        createDayCalendar(calendar, config[0], events[0]);
+      else
+        createFullCalendar(calendar, config[0], events[0]);
+    }
+  );
+}
+
+function createFullCalendar(calendar, config, allEvents) {
+  // there's a strong assumption here that all parallel sessions are aligned on start/end time
+  let sessions_by_day_and_time = d3.group(
+    allEvents,
+    d => d.start.split("T")[0],
+    d => d.start.split("T")[1],
+  );
+
+  for (const dayKey of sessions_by_day_and_time.keys()) {
+    const dayEvents = sessions_by_day_and_time.get(dayKey);
+
+    for (const timeslotKey of dayEvents.keys()) {
+      // fill in positioning based on first session (should be grouped already)
+      const sessions = dayEvents.get(timeslotKey);
+      const dayPosition = sessions[0].day + " / auto";
+      const timePosition = sessions[0].timeStart + " / " + sessions[0].timeEnd;
+
+      const timeslot = calendar.append('div')
+        .style('grid-column', dayPosition)
+        .style('grid-row', timePosition)
+        .classed('session-group', sessions.length !== 1);
+
+      if (sessions.length === 1) {
+        const session = sessions[0];
+        const session_color = config.calendar.colors[session.eventType];
+        timeslot.attr('class', 'session')
+          .attr('id', session.id)
+          .style('background-color', session_color)
+          .style('color', getTextColorByBackgroundColor(session_color))
+          .text(session.title);
+      }
+      else {
+        timeslot.selectAll('.session')
+          .data(sessions, d => d.id)
+          .join('div')
+            .attr('class', 'session')
+            .attr('data-toggle', 'popover')
+            .attr('data-content', d => d.title)
+            .style('background-color', d => config.calendar.colors[d.eventType])
+            .style('color', d => getTextColorByBackgroundColor(config.calendar.colors[d.eventType]))
+            // .text(d => d.title);
+      }
+    };
+  };
+}
+
+function createDayCalendar(calendar, config, dayEvents) {
+  const getColor = d => config.calendar.colors[d.eventType];
+  const getTextColor = d => getTextColorByBackgroundColor(getColor(d));
+
+  calendar.selectAll(".session")
+    .data(dayEvents)
+    .join("div")
+      .attr("class", "session")
+      .attr('data-toggle', 'popover')
+      .attr('data-content', d => d.title)
+      .style('grid-column', d => `room-${d.room}-start / auto`)
+      .style('grid-row', d => `${d.timeStart} / ${d.timeEnd}`)
+      .style('background-color', getColor)
+      .style('color', getTextColor)
+      .text(d => d.title)
+}
+
+function populateDays(calendarSelection) {
+  let dayData = [
+    "Sun, Oct 24",
+    "Mon, Oct 25",
+    "Tue, Oct 26",
+    "Wed, Oct 27",
+    "Thu, Oct 28",
+    "Fri, Oct 29",
+  ];
+
+  populateHeader(calendarSelection, dayData, true /* isDay */);
+}
+
+function populateRooms(calendarSelection) {
+  // TODO: use room names
+  let roomData = [
+    "Room 1",
+    "Room 2",
+    "Room 3",
+    "Room 4",
+    "Room 5",
+    "Room 6",
+    "Room 7",
+  ];
+
+  populateHeader(calendarSelection, roomData);
+}
+
+function populateHeader(calendarSelection, data, isDay) {
+  let columnPosition = (_d, i) => `${(isDay ? 'day' : 'room')}-${i+1} / auto`;
+
+  calendarSelection.selectAll(".day-slot")
+    .data(data, d => d)
+    .join("div")
+      .attr("class", "day-slot")
+      .style("grid-row", "tracks / auto")
+      .style("grid-column", columnPosition)
+      .append('h2')
+        .text(d => d);
+}
+
+function populateTimes(calendarSelection) {
+  let timeData = [
+    ["7:30 AM", "time-0730"],
+    ["8:00", "time-0800"],
+    ["9:30", "time-0930"],
+    ["10:00", "time-1000"],
+    ["11:30", "time-1130"],
+    ["12:00 PM", "time-1200"],
+    ["1:30", "time-1330"],
+    ["2:00 PM", "time-1400"],
+    ["3:00", "time-1500"],
+    ["3:30", "time-1530"],
+  ];
+
+  calendarSelection.selectAll(".time-slot:not(.converted-timezone)")
+    .data(timeData, d => d[0])
+    .join("h2")
+      .attr("class", "time-slot")
+      .attr("data-time", d => d[1])
+      .style("grid-row", d => d[1])
+      .text(d => d[0]);
+
+  calendarSelection.selectAll(".converted-timezone")
+    .data(timeData, d => d[0])
+    .join("h2")
+      .attr("class", "time-slot converted-timezone")
+      .attr("data-time", d => d[1])
+      .style("grid-row", d => d[1])
+      .text(d => d[0]);
+}
+
+function resetCalendar() {
+  d3.selectAll('.session-group').remove();
+  d3.selectAll('.session').remove();
+}
+
 function updateTimezone() {
   // get timezone
   const timezone = getTimezone();
@@ -103,7 +281,7 @@ function updateTimezone() {
   // apply timezone
   $('.converted-timezone').each((_, e) => {
     const element = $(e);
-    const hourminutes = element.attr('data-time');
+    const hourminutes = element.attr('data-time').split('-')[1];
 
     let time = moment(`2021-10-25 ${hourminutes.slice(0, 2)}:${hourminutes.slice(2, 4)} -05:00`, "YYYY-MM-DD HH:mm ZZ");
 
