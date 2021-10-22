89abe3a7ee8478dc6fec205d05522499b8186514
Dylan Cashman <depcashman@gmail.com>
2021-10-22 01:36:56 -0400

    Fixes #68

--- static/2021/js/modules/gdprCookies.js
+++ static/2021/js/modules/gdprCookies.js
@@ -1,9 +1,15 @@
-const allow_cookies = Cookies.get("miniconf-allow-cookies");
+const allow_cookies = Cookies.get("miniconf-allow-cookies-2021");
 
 if (!allow_cookies) {
   $(".gdpr").show();
 }
+
+if (allow_cookies) {
+  _paq.push(['setConsentGiven']);
+  _paq.push(['setCookieConsentGiven']);
+}
+
 $(".gdpr-btn").on("click", () => {
-  Cookies.set("miniconf-allow-cookies", 1, { expires: 7 });
+  Cookies.set("miniconf-allow-cookies-2021", 1, { expires: 7 });
   $(".gdpr").hide();
 });
