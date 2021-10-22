911f1dc07aa43c3cfbbe158656d514d5a61b4290
Dylan Cashman <depcashman@gmail.com>
2021-10-22 10:30:45 -0400

    Add option to reject cookie tracking

--- static/2021/js/modules/gdprCookies.js
+++ static/2021/js/modules/gdprCookies.js
@@ -1,6 +1,7 @@
 const allow_cookies = Cookies.get("miniconf-allow-cookies-2021");
+const reject_cookies = Cookies.get("miniconf-reject-cookies-2021");
 
-if (!allow_cookies) {
+if (!allow_cookies && !reject_cookies) {
   $(".gdpr").show();
 }
 
@@ -9,7 +10,12 @@ if (allow_cookies) {
   _paq.push(['setCookieConsentGiven']);
 }
 
-$(".gdpr-btn").on("click", () => {
+$("#gdpr-btn-accept").on("click", () => {
   Cookies.set("miniconf-allow-cookies-2021", 1, { expires: 7 });
+  $(".gdpr").hide();
+});
+
+$("#gdpr-btn-reject").on("click", () => {
+  Cookies.set("miniconf-reject-cookies-2021", 1, { expires: 7 });
   $(".gdpr").hide();
 });
