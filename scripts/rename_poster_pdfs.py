bec74cd794d1c54eb70c37a66cd0146b32b7a371
Dylan Cashman <depcashman@gmail.com>
2022-10-07 23:25:33 -0400

    update poster script to generate small thumbnails

--- scripts/rename_poster_pdfs.py
+++ scripts/rename_poster_pdfs.py
@@ -4,35 +4,42 @@ import glob
 import shutil
 import re
 import os
+import subprocess
 # DIRECTORY_PATH = 'a-biomedvischallenge-posters'
 # DIRECTORY_PATH = 'a-ldav-posters'
 # DIRECTORY_PATH = 'a-sciviscontest-posters'
-# DIRECTORY_PATH = 'a-vast-posters'
+DIRECTORY_PATH = 'a-vast-posters'
 # DIRECTORY_PATH = 'a-vizsec-posters'
-DIRECTORY_PATH = 'v-vis-posters'
+# DIRECTORY_PATH = 'v-vis-posters'
 cwd = os.getcwd()
 
 for filename in glob.glob(DIRECTORY_PATH + "/*"):
 	name, typ = filename.split("/")[-1].split(".")
-	# print("filename is ", filename, " name is ", name, " and typ ", typ)
+	paper_id = re.findall('\d{4}', name )[0]
+	print("filename is ", filename, " name is ", name, " and typ", typ)
 	# name, typ = filename.split(".")
 
-	# For biomedvischallenge
-	if typ == 'pdf':
-		paper_id = re.findall('\d+', name )[0]
-		dst = cwd + '/to_upload/' + DIRECTORY_PATH + '-' + paper_id + '.pdf'
-		# print("pdf found dst is ", dst)
-		shutil.copyfile(filename, dst)
-
-	# For all others
 	if 'doc' in name:
-		paper_id = re.findall('\d{4}', name )[0]
 		dst = cwd + '/to_upload/' + DIRECTORY_PATH + '-' + paper_id + '-summary.pdf'
 		# print("pdf found dst is ", dst)
 		shutil.copyfile(filename, dst)
 
 	if ('file-i9' in name) or ('file-i10' in name) or ('file-i17' in name):
-		paper_id = re.findall('\d{4}', name )[0]
 		dst = cwd + '/to_upload/' + DIRECTORY_PATH + '-' + paper_id + '.pdf'
 		# print("pdf found dst is ", dst)
 		shutil.copyfile(filename, dst)
+
+	if typ == 'png':
+		dst = cwd + '/to_upload/paper_images_small/' + DIRECTORY_PATH + '-' + paper_id + '.png'
+		cmd = 'convert ' + filename + ' -resize 600x ' + str(dst)
+		print("for imagemagick, dst is ", dst)
+		print(cmd)
+		subprocess.call(cmd, shell=True)
+
+	# For biomedvischallenge
+	if typ == 'pdf':
+		dst = cwd + '/to_upload/' + DIRECTORY_PATH + '-' + paper_id + '.pdf'
+		# print("pdf found dst is ", dst)
+		shutil.copyfile(filename, dst)
+
+	
